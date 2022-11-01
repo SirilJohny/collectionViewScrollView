72d62c9657e1504788053797865ec0fff9c370b6
SIRILJOHNY <cyriljohny508@gmail.com>
2022-11-01 09:43:57 +0530

    Initial Commit

---
+++ collectionViewScrollView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  collectionViewScrollView
+//
+//  Created by DDUKK on 01/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
